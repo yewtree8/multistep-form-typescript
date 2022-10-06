34320805abdf405786258dae9d4d2f6935519729
Mat <matgallimore@romainberg.com>
2022-10-06 12:27:04 +0100

    feat(master) Added custom step hook

--- src/hooks/useMultiStepForm.ts
+++ src/hooks/useMultiStepForm.ts
@@ -2,4 +2,25 @@ import { ReactElement, useState } from "react";
 
 export function useMultistepForm(steps: ReactElement[]) {
     const [stepIndex, setStepIndex] = useState(0);
+
+
+const next = () => {
+    setStepIndex((step) => { return step+1})
+}
+
+const back = () => { 
+    setStepIndex((step) => { return step-1})
+}
+
+function goToStep(step:number) {
+    setStepIndex(step);
+}
+
+
+return { 
+    stepIndex,
+    step: steps[stepIndex],
+    goToStep
+}
+
 }
